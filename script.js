8eb7af7addfac4d7568d85c57a1675a1ddfd6830
pateldharm0158-tech <pateldharm0158@gmail.com>
2026-06-09 11:43:10 +0530

    Restore car data and rendering functions

--- script.js
+++ script.js
@@ -5,21 +5,21 @@
   name: "2024 Mahindra XUV700 AX7",
   price: "₹22.50 Lakh",
   numericPrice: 2250000,
-  img: "xuv 700/xuv 1.png",
+  img: "xuv700/xuv 1.png",
   year: "2024",
   km: "12,000 km",
   fuel: "Diesel",
   trans: "Automatic",
 
 images: [
-  "xuv 700/xuv 1.png",
-  "xuv 700/xuv 2.png",
-  "xuv 700/xuv 3.png",
-  "xuv 700/xuv 4.png",
-  "xuv 700/xuv 5.png",
-  "xuv 700/xuv 6.png",
-  "xuv 700/xuv 8.png",
-  "xuv 700/xuv 9.png"
+  "xuv700/xuv 1.png",
+  "xuv700/xuv 2.png",
+  "xuv700/xuv 3.png",
+  "xuv700/xuv 4.png",
+  "xuv700/xuv 5.png",
+  "xuv700/xuv 6.png",
+  "xuv700/xuv 8.png",
+  "xuv700/xuv 9.png"
 ]
 },
       {
