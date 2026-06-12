9c9d9006e34f5b6834d48ba3f9eb3624a8e168dd
pateldharm0158-tech <pateldharm0158@gmail.com>
2026-06-12 12:42:55 +0530

    Initialize Firebase app and setup authentication

--- firebase.js
+++ firebase.js
@@ -1,1 +1,40 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
+import {
+getAuth,
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword,
+signOut
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+
+apiKey: "PASTE_YOUR_API_KEY",
+
+authDomain: "PASTE_YOUR_AUTH_DOMAIN",
+
+projectId: "magneto-carsz",
+
+storageBucket: "PASTE_YOUR_STORAGE_BUCKET",
+
+messagingSenderId: "PASTE_YOUR_SENDER_ID",
+
+appId: "PASTE_YOUR_APP_ID"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+export const db = getFirestore(app);
+
+export {
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword,
+signOut
+};
