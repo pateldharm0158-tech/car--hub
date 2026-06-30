69a53fa07cc68d01533372bd94beda026fe97f15
pateldharm0158-tech <pateldharm0158@gmail.com>
2026-06-30 12:28:06 +0530

    Implement custom popup functionality
    Added showPopup and closePopup functions to manage custom popups.

--- popup.js
+++ popup.js
@@ -1,1 +1,92 @@
+function showPopup(title, message, redirect = null) {
 
+let oldPopup = document.getElementById("customPopup");
+
+if (oldPopup) oldPopup.remove();
+
+const popup = document.createElement("div");
+
+popup.id = "customPopup";
+
+popup.innerHTML = `
+<div style="
+position:fixed;
+top:0;
+left:0;
+width:100%;
+height:100%;
+background:rgba(0,0,0,.6);
+display:flex;
+justify-content:center;
+align-items:center;
+z-index:999999;
+">
+
+<div style="
+background:#fff;
+width:420px;
+max-width:90%;
+border-radius:25px;
+padding:35px;
+text-align:center;
+box-shadow:0 15px 40px rgba(0,0,0,.3);
+position:relative;
+animation:popup .3s ease;
+">
+
+<span onclick="closePopup('${redirect}')"
+style="
+position:absolute;
+right:20px;
+top:10px;
+font-size:35px;
+cursor:pointer;
+">&times;</span>
+
+<h2 style="
+font-size:38px;
+font-weight:bold;
+margin-bottom:20px;
+">
+${title}
+</h2>
+
+<p style="
+font-size:20px;
+margin-bottom:30px;
+">
+${message}
+</p>
+
+<button
+onclick="closePopup('${redirect}')"
+style="
+background:#dc2626;
+color:white;
+border:none;
+padding:12px 40px;
+border-radius:12px;
+font-size:18px;
+cursor:pointer;
+">
+OK
+</button>
+
+</div>
+
+</div>
+`;
+
+document.body.appendChild(popup);
+
+}
+
+function closePopup(redirect){
+
+document.getElementById("customPopup").remove();
+
+if(redirect){
+window.location = redirect;
+}
+
+}
